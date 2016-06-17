398622e134f752e5369c50c8a56846ef96829dce
pauliracane <smallmaddog11@yahoo.com>
2016-06-17 09:29:56 -0400

    Default combatants table works.

--- safetydome.py
+++ safetydome.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 from flask import Flask, abort, request, render_template
+import psycopg2
 
 app = Flask(__name__)
+
+con = psycopg2.connect("dbname='safetydome' user='flask' password='flask' host='localhost'")
+cur = con.cursor()
 
 @app.route('/')
 def index():
@@ -13,7 +17,19 @@ def index():
 @app.route('/combatant/<identifier>')
 def combatant(identifier=None):
     #give them combatants.html
-    return render_template('combatants.html')
+    cur.execute('SELECT combatant.name, combatant.id, species.name FROM public.combatant, public.species WHERE combatant.species_id = species.id;')
+    combatants = cur.fetchall()
+    class Combatant:
+        def __init__(self, name, ident, species):
+            self.name = name
+            self.ident = ident
+            self.species = species
+    TheRealCombatants = []
+    for each in combatants:
+        x = Combatant(each[0], each[1],each[2])
+        TheRealCombatants.append(x)
+
+    return render_template('combatants.html', combatants = TheRealCombatants)
 
 @app.route('/battle/')
 @app.route('/battle/<identifier>')
@@ -27,7 +43,7 @@ def results():
     return render_template('results.html')
 
 if __name__ == '__main__':
-    app.run(debug=False, port=8056)
-    
+    #Connect to database
+    app.run(debug=True, port=8056)
 
 
